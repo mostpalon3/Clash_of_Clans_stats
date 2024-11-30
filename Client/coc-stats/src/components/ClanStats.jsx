7d28e519c6db2f6c26248109be86805b26f72da4
Sumit Sagar <sumitsagar2612@gmail.com>
2024-11-30 05:41:01 +0530

    added

---
+++ Client/coc-stats/src/components/ClanStats.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+const ClanStats = () => {
+  const { tag } = useParams();
+  const [clan, setClan] = useState(null);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/clan/${tag}`)
+      .then((response) => setClan(response.data))
+      .catch((error) => console.error(error));
+  }, [tag]);
+
+  if (!clan) return <div>Loading...</div>;
+
+  return (
+    <div className="p-5 bg-white shadow rounded">
+      <h2 className="text-2xl font-bold">Clan: {clan.name}</h2>
+      <p>Level: {clan.clanLevel}</p>
+      <p>War Wins: {clan.warWins}</p>
+      <p>Members: {clan.members}</p>
+    </div>
+  );
+};
+
+export default ClanStats;
